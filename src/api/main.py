2fbf6459fa98858f4f31b631c98ffc1997e77096
XxHurtadoxX <danis.hg@hotmail.com>
2025-10-16 20:24:24 -0500

    url

--- src/api/main.py
+++ src/api/main.py
@@ -42,7 +42,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://localhost:3001",
         "http://127.0.0.1:3000",
-        "https://*.netlify.app",  # Temporal - actualizar con URL específica
+        "https://moneave.netlify.app/",  # Temporal - actualizar con URL específica
     ],
     allow_credentials=True,
     allow_methods=["*"],
